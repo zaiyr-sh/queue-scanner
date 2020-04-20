2000695e7391b0dabf3abd3248ced22b27487ab0
Zaiyr <zaiyr.00@gmail.com>
2020-04-21 02:06:18 +0600

    Security Service customization

--- src/main/java/com/intro/web/webproject/Service/SecurityService.java
+++ src/main/java/com/intro/web/webproject/Service/SecurityService.java
@@ -12,9 +12,9 @@ public class SecurityService extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().
                 authorizeRequests()
-                .antMatchers("/", "/api/client", "/api/client/all", "/api/client/ip/", "/api/client/ip/{ipAddress}",
-                        "/api/employee", "/api/employee/{id}", "/api/employee/all", "/api/employee/queue",
-                        "/api/queue", "/api/queue/all", "/api/queue/{id}").permitAll()
+                .antMatchers("/", "/api/client/", "/api/client", "/api/client/all/", "/api/client/all", "/api/client/ip/", "/api/client/ip", "/api/client/ip/{ipAddress}", "/api/client/ip/{ipAddress}/",
+                        "/api/employee/", "/api/employee", "/api/employee/{id}", "/api/employee/{id}/", "/api/employee/all/", "/api/employee/all", "/api/employee/queue/", "/api/employee/queue",
+                        "/api/queue/", "/api/queue", "/api/queue/all/", "/api/queue/all", "/api/queue/{id}", "/api/queue/{id}/").permitAll()
                 .antMatchers("/**").access("hasRole('Employers Administration')")
                 .and()
                 .formLogin();
