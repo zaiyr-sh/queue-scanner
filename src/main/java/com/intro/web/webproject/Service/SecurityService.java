4a9a69929de5ed8bc8442722af422df6b8087a40
Zaiyr <zaiyr.00@gmail.com>
2020-04-21 01:51:14 +0600

    Security Service customization

--- src/main/java/com/intro/web/webproject/Service/SecurityService.java
+++ src/main/java/com/intro/web/webproject/Service/SecurityService.java
@@ -12,7 +12,9 @@ public class SecurityService extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().
                 authorizeRequests()
-                .antMatchers("/", "/api/client", "/api/employee", "/api/queue").permitAll()
+                .antMatchers("/", "/api/client", "/api/client/all", "/api/client/ip/", "/api/client/ip/{ipAddress}",
+                        "/api/employee", "/api/employee/{id}", "/api/employee/all", "/api/employee/queue",
+                        "/api/queue", "/api/queue/all", "/api/queue/{id}").permitAll()
                 .antMatchers("/**").access("hasRole('Employers Administration')")
                 .and()
                 .formLogin();
